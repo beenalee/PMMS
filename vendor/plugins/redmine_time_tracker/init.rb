3a877a64a68dc0a6364a67344a7f3c344ac9dc01
Beena <beenareyes@gmail.com>
2011-12-06 00:53:45 +0800

    added timetracker as plain content

---
+++ vendor/plugins/redmine_time_tracker/init.rb
@@ -0,0 +1,26 @@
+require 'redmine'
+
+require_dependency 'time_tracker_hooks'
+
+Redmine::Plugin.register :redmine_time_tracker do
+    name 'Redmine Time Tracker plugin'
+    author 'Jérémie Delaitre'
+    description 'This is a plugin to track time in Redmine'
+    version '0.4'
+
+    requires_redmine :version_or_higher => '1.1.0'
+
+    settings :default => { 'refresh_rate' => '60', 'status_transitions' => {} }, :partial => 'settings/time_tracker'
+
+    permission :view_others_time_trackers, :time_trackers => :index
+    permission :delete_others_time_trackers, :time_trackers => :delete
+
+    menu :account_menu, :time_tracker_menu, '',
+        {
+            :caption => '',
+            :html => { :id => 'time-tracker-menu' },
+            :first => true,
+            :param => :project_id,
+            :if => Proc.new { User.current.logged? }
+        }
+end
